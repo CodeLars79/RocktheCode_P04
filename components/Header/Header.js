998b56ccd109dee6d84217d4d9dccd0751f71deb
Lars Sorensen <161650519+CodeLars79@users.noreply.github.com>
2025-04-16 19:15:09 +0200

    Add files via upload

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,21 +3,22 @@ import './Header.css'
 const template = () => {
   return `
   <header>
-  <a href="#top" class="logo-link">
-    <img src="./assets/logo.png" alt="Logo" class="logo" /></a>
+    <a href="#top" class="logo-link">
+      <img src="./assets/logo.png" alt="Logo" class="logo" />
+    </a>
     <nav>
       <ul>
         <li>
-          <a href="#aboutme">About me</a>
+          <a href="#aboutme" class="scroll-link">About me</a>
         </li>
         <li>
-          <a href="#education">Education</a>
+          <a href="#education" class="scroll-link">Education</a>
         </li>
         <li>
-          <a href="#experience">Experience</a>
+          <a href="#experience" class="scroll-link">Experience</a>
         </li>
         <li>
-          <a href="#projects">Projects</a>
+          <a href="#projects" class="scroll-link">Projects</a>
         </li>
       </ul>
     </nav>
@@ -30,3 +31,23 @@ const Header = () => {
 }
 
 export default Header
+
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('.scroll-link')
+
+  links.forEach((link) => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault()
+
+      const targetId = this.getAttribute('href').substring(1)
+      const targetElement = document.getElementById(targetId)
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - 150,
+          behavior: 'smooth'
+        })
+      }
+    })
+  })
+})
